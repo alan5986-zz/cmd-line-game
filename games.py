db529c3f128407d9caa41bbd908edf5121fed007
alan5986 <51156343+alan5986@users.noreply.github.com>
2019-08-15 20:25:28 +0100

    add word chain game

--- games.py
+++ games.py
@@ -11,6 +11,11 @@ with open('words.csv', 'rb') as csvFile:
             word = row["#words"]
             words.append(word)           
 
+moreWords = []
+txt = open('words.txt', 'r').read().splitlines()
+for line in txt:
+    moreWords.append(line)
+    
 #function to print out phrases one letter at a time on the same line            
 def say(phrase):
     out = ""
@@ -20,6 +25,72 @@ def say(phrase):
         sys.stdout.flush()
         time.sleep(0.03)
     print("")
+
+#word chain game
+def wordchain():
+    play = True
+    while play:
+
+        #construct word chain
+        chain = []
+        random.shuffle(moreWords)
+        currentWord = random.choice(moreWords)
+        chain.append(currentWord)
+        for word in moreWords:
+            count = 0
+            if len(word) == len(currentWord):
+                for i in range(len(currentWord)):
+                    if word[i] != currentWord[i]:
+                        count = count + 1
+                if count == 1 and word not in chain:
+                    chain.append(word)
+                    currentWord = word
+                    
+        if len(chain) == 5:
+
+            #construct answer list
+            answer = []
+            blank = ""
+            for i in range(len(currentWord)):
+                blank = blank + '-'
+            answer.append(chain[0])
+            for i in range(1,4):
+                answer.append(blank)
+            answer.append(chain[4])
+            
+            #gameplay
+            guessing = True
+            say("Welcome to Word Chain. Each word is different by one letter.")
+            say("Guess words to complete the chain. Type 'give up' to see the solution.")
+            print("")
+            while guessing:
+                for i in answer:
+                    print i
+                print("")
+                guess = raw_input()
+                if guess == 'give up':
+                    guessing = False
+                    say("Hard luck! The solution was:")
+                    for i in chain:
+                        print i        
+                elif guess not in chain:
+                    say("...Nope")
+                    print("")
+                else:
+                    say("...Correct!")
+                    print("")
+                    for i in range(len(answer)):
+                        if guess == chain[i]:
+                            answer[i] = chain[i]
+                    if answer == chain:
+                        guessing = False
+                        say("Congrats you win!")
+                        for i in answer:
+                            print i
+            again = raw_input("Play again? y or n. \n")
+            if again == 'n':
+                play = False
+                    
 
 #hangman game
 def hangman():
@@ -107,17 +178,31 @@ say("Welcome to Beany Games!")
 playing = True
 
 while playing:
-    say("Type a number to choose a game:(1)Guess (2)Hangman or (0)Quit")
+    say("Type a number to choose a game:(1)Guess (2)Hangman (3)Word Chain or (0)Quit")
     game = input()
     if game == 1:
         guess()
     elif game == 2:
         hangman()
+    elif game == 3:
+        wordchain()
     elif game == 0:
         say("Thanks for playing, bye!")
         playing = False
     
     
+
+
+
+                    
+
+
+
+
+
+
+
+
     
 
 
